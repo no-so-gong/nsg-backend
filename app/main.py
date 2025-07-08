54ea20501eda6b4651bd903cb86a57eec9a3b5de
ChaeHyunLim <chaehyun010104@gmail.com>
2025-07-08 13:50:06 +0900

    fix: solve ai predict and chage emotion_model.pkl

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import hello
+from app.routers import predict
 
 app = FastAPI()
 
@@ -13,3 +14,4 @@ app.add_middleware(
 )
 
 app.include_router(hello.router)
+app.include_router(predict.router)
--- app/services/predictor.py
+++ app/services/predictor.py
@@ -7,11 +7,20 @@ from app.schemas.input import EmotionInput
 # .env 파일 로드
 load_dotenv()
 
-# 환경 변수에서 모델 경로 읽기
+# 모델 경로 설정
 model_path = os.getenv("MODEL_PATH", "./model/emotion_model.pkl")
+
+# 모델 불러오기
 model = joblib.load(model_path)
 
 def predict_emotion(input_data: EmotionInput) -> float:
-    df_input = pd.DataFrame([input_data.dict()])
-    prediction = model.predict(df_input)[0]
-    return round(prediction, 2)
+    # 16개의 모든 필드를 그대로 받아서 예측에 사용
+    data = input_data.dict()
+
+    # DataFrame으로 변환
+    df_input = pd.DataFrame([data])
+
+    # 컬럼 순서 맞추기 필요 없음 -> validate_features=False 옵션 추가!
+    prediction = model.predict(df_input, validate_features=False)[0]
+
+    return float(prediction)
