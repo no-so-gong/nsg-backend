eeda6ef901a0038adce0de78c94e713e9dbd0cb9
김태연 <te4939@naver.com>
2025-08-06 16:08:12 +0900

    fix: db 분리 및 예외처리

--- app/api/event/controller.py
+++ app/api/event/controller.py
@@ -4,13 +4,15 @@ from app.core.database import get_db
 from app.api.event.service import get_attendance_data, check_in_attendance
 from app.api.event.schema import AttendanceResponseData, AttendanceResponse
 from uuid import UUID
+from app.core.exception import CustomException
+import traceback
 
 router = APIRouter(prefix="/api/v1/event", tags=["event"])
 
 @router.get("/attendance", response_model=AttendanceResponse)
 def attendance_info(user_id: UUID = Header(..., alias="user-id"), db: Session = Depends(get_db)):
     if not user_id:
-        raise HTTPException(status_code=401, detail="user-id 헤더가 누락되었거나 유효하지 않습니다.")
+        raise CustomException(message = "user-id 헤더가 누락되었거나 유효하지 않습니다.", status=401)
 
     try:
         data = get_attendance_data(user_id, db)
@@ -19,17 +21,18 @@ def attendance_info(user_id: UUID = Header(..., alias="user-id"), db: Session = 
             "message": "출석 정보 조회 성공",
             "data": data
         }
-    except HTTPException:
+    except CustomException:
         raise
     except Exception:
-        raise HTTPException(status_code=500, detail="서버 내부 오류가 발생했습니다.")
+        traceback.print_exc() 
+        raise CustomException(message = "서버 내부 오류가 발생했습니다.", status=500)
 
 
 @router.post("/attendance/checkin", response_model=AttendanceResponse)
 def attendance_checkin(user_id: UUID  = Header(..., alias="user-id"), db: Session = Depends(get_db)):
     if not user_id:
-        raise HTTPException(status_code=401, detail="user-id 헤더가 누락되었거나 유효하지 않습니다.")
-    
+        raise CustomException(message = "user-id 헤더가 누락되었거나 유효하지 않습니다.", status=401)
+
     try:
         data = check_in_attendance( user_id, db)
         return {
@@ -37,7 +40,7 @@ def attendance_checkin(user_id: UUID  = Header(..., alias="user-id"), db: Sessio
             "message": "출석이 완료되었습니다.",
             "data": data
         }
-    except HTTPException as e:
+    except CustomException as e:
         raise e
     except Exception:
-        raise HTTPException(status_code=500, detail="서버 내부 오류가 발생했습니다.")
+        raise CustomException(message = "서버 내부 오류가 발생했습니다.", status=500)
