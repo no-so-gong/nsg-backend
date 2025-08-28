771fd44023f3d193f2b88a2e2c956bbffda5fe79
ChaeHyunLim <chaehyun010104@gmail.com>
2025-08-28 15:28:24 +0900

    8/28 my checkpoint

--- app/api/care/controller.py
+++ app/api/care/controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from app.api.care.schema import MLInput
 from app.api.care.service import predict_emotion
 
-router = APIRouter(prefix="/api/v1/care", tags=["Care"])
+router = APIRouter(prefix="/api/v1/cares", tags=["Care"])
 
 @router.post("/action") # 기존 /predict 테스트 api 이름만 변경함. 이후에 작업 다시
 def get_emotion_prediction(input_data: MLInput):
